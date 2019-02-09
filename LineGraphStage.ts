de2139e7ca4b4de705f60f2bad8c8d9dddec19a2
Anwesh43 <anweshthecool0@gmail.com>
2019-02-09 13:08:05 +0530

    adding scale methods

--- LineGraphStage.ts
+++ LineGraphStage.ts
@@ -7,3 +7,24 @@ const strokeFactor : number = 60
 const lineColor : string = "#4CAF50"
 const backColor : string = "#212121"
 const data : Array<number> = [10, 20, 30, 5, 15, 60, 35, 25]
+
+const maxScale : Function = (scale : number, i : number, n : number) : number => {
+    return Math.max(0, scale - i / n)
+}
+
+const divideScale : Function = (scale : number, i : number, n : number) : number => {
+    return Math.min(1 / n, maxScale(scale, i, n)) * n
+}
+
+const scaleFactor : Function = (scale : number) : number => {
+    return Math.floor(scale / scDiv)
+}
+
+const mirrorValue : Function = (scale : number, a : number, b : number) : number => {
+    const k = scaleFactor(scale)
+    return (1 - k) / a + k / b
+}
+
+const updateValue : Function = (scale : number, dir : number, a : number, b : number) : number => {
+    return mirrorValue(scale, a, b) * dir * scGap
+}
